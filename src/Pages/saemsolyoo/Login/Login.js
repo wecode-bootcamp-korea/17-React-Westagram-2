36a892e283028d6245cb25c8b366bbe116505d87
saemsol <yoosaemsol@gmail.com>
2021-01-27 22:44:55 +0900

    Add: 클론 후 개인 작성코드 옮기기 완료

--- src/Pages/saemsolyoo/Login/Login.js
+++ src/Pages/saemsolyoo/Login/Login.js
@@ -1,13 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./Login.scss";
+import { withRouter } from "react-router-dom";
 
 class Login extends Component {
-    render() {
-        return (
-            <div>
+  goToMain = () => {
+    this.props.history.push("/main-saemsol");
+    console.log(this.props);
+    console.log(this.props.history);
+  };
 
+  render() {
+    return (
+      <div className="login-page">
+        <section className="log-in">
+          <h1>Westagram</h1>
+          <form className="login-form">
+            <input
+              className="input-id"
+              type="text"
+              placeholder="전화번호, 사용자 이름 또는 이메일"
+              required
+            />
+            <input
+              className="input-pw"
+              type="password"
+              placeholder="비밀번호"
+              required
+            />
+            <a onClick={this.goToMain}>
+              <button className="login-btn" type="button">
+                로그인
+              </button>
+            </a>
+            <div className="divider">
+              <span>또는</span>
             </div>
-        );
-    }
+            <a className="log-in-facebook" href="#">
+              <i className="fab fa-facebook-square"></i>
+              <span>Facebook으로 로그인</span>
+            </a>
+          </form>
+          <span className="forgot-pw">비밀번호를 잊으셨나요?</span>
+        </section>
+        <section className="join">
+          <span>
+            계정이 없으신가요? <a href="#">가입하기</a>
+          </span>
+        </section>
+      </div>
+    );
+  }
 }
 
-export default Login;
+export default withRouter(Login);
