02f4773e1eb37c395a50735c6995588df1f69e70
rosarang <72490935+rosarang@users.noreply.github.com>
2021-01-28 23:58:42 +0900

    Add: Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/Pages/saranglee/Login/Login.js
+++ src/Pages/saranglee/Login/Login.js
@@ -8,11 +8,13 @@ import './Login.scss';
 import westagramLogo from '../../../images/saranglee/Login/westagramLogo.svg';
 
 class LoginSarang extends Component {
+
   constructor() {
     super();
     this.state = {
       idValue: '',
-      pwValue: ''
+      pwValue: '',
+      btnOpacity: 0.3
     }
   }
 
@@ -25,7 +27,11 @@ class LoginSarang extends Component {
   handlePwInput = (event) => {
     this.setState({
       pwValue: event.target.value
-    })
+    });
+  }
+
+  validation = () => {
+    this.state.idValue.includes('@') && this.state.pwValue.length >= 5 ? this.setState({btnOpacity: 1}) : this.setState({btnOpacity: 0.3});
   }
 
   goToMain = () => {
@@ -37,10 +43,24 @@ class LoginSarang extends Component {
       <main className="LoginSarang">
         <div className="logo-login-wrap">
           <img className="logo-img" alt="Westagram Logo" src={ westagramLogo } />
-          <div className="login-container">
-            <input className="id-input" onChange={this.handleIdInput} type="text" placeholder="전화번호, 사용자 이름 또는 이메일" />
-            <input className="pw-input" onChange={this.handlePwInput} type="password" placeholder="비밀번호" />
-            <button className="login-btn disabled" onClick={this.goToMain}>로그인</button>
+          <div className="login-container" onChange={this.validation}>
+            <input 
+              className="id-input"
+              onChange={this.handleIdInput}
+              type="text"
+              placeholder="전화번호, 사용자 이름 또는 이메일"
+            />
+            <input 
+              className="pw-input"
+              onChange={this.handlePwInput}
+              type="password"
+              placeholder="비밀번호"
+            />
+            <button 
+              style={{opacity: this.state.btnOpacity}} 
+              className="login-btn disabled" 
+              onClick={this.goToMain}>로그인
+            </button>
           </div>
         </div>
         <a className="forgot-pw" href="https://www.instagram.com/accounts/password/reset">비밀번호를 잊으셨나요?</a>
