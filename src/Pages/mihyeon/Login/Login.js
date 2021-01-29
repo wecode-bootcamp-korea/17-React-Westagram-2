2d6e0092d3d1b7bdaaaa7d88066c60c336459abe
mia <cocoball200@gmail.com>
2021-01-29 17:41:31 +0900

    Add : Mission 3 - 댓글 기능 구현

--- src/Pages/mihyeon/Login/Login.js
+++ src/Pages/mihyeon/Login/Login.js
@@ -14,9 +14,18 @@ class Login extends React.Component {
     handleChange = (e) => {
         this.setState({
             [e.target.name]: e.target.value
-        })
+        });
         const validation = this.state.id.indexOf("@") && this.state.password >= 5
         validation ? this.setState({ opacity: "1" }) : this.setState({ opacity: "0.3" });
+    }
+    goToValidate = (e) => {
+        this.setState({
+            validate: this.state.password === "123456" && this.state.id === "cocoball200@gmail.com"
+        })
+        this.state.validate ? this.props.history.push("/main-miheyon") : this.props.history.push("/login-miheyon")
+    }
+    onKeyPress = (e) => {
+        if (e.key === 'Enter') this.goToValidate();
     }
 
     render() {
@@ -28,9 +37,9 @@ class Login extends React.Component {
                         <h1>Westagram</h1>
 
                         <div className="input_container">
-                            <input className="input_id" type="text" name="id" placeholder="전화번호, 사용자 이름 또는 이메일" value={this.state.id} onChange={this.handleChange} />
-                            <input className="input_password" type="password" name="password" placeholder="비밀번호" value={this.state.password} onChange={this.handleChange} onKeyPress={this.onKeyPress} />
-                            <input className="login_button" type="button" value="로그인" style={{ opacity: this.state.opacity }} />
+                            <input className="input_id" type="text" name="id" placeholder="전화번호, 사용자 이름 또는 이메일" defaultValue={this.state.id} onChange={this.handleChange} />
+                            <input className="input_password" type="password" name="password" placeholder="비밀번호" defaultValue={this.state.password} onChange={this.handleChange} onKeyPress={this.onKeyPress} />
+                            <input className="login_button" type="button" defaultValue="로그인" style={{ opacity: this.state.opacity }} onClick={this.goToValidate} />
                         </div>
 
                         <div className="or_container">
@@ -40,8 +49,8 @@ class Login extends React.Component {
                         </div>
 
                         <div className="facebook_login_link">
-                            <a className="facebook_login_text" href="#"><i className="fab fa-facebook-square"></i>Facebook으로 로그인</a>
-                            <a href="#" tabindex="0">비밀번호를 잊으셨나요?</a>
+                            <a className="facebook_login_text" href="/"><i className="fab fa-facebook-square"></i>Facebook으로 로그인</a>
+                            <a href="/">비밀번호를 잊으셨나요?</a>
                         </div>
 
                     </div>
@@ -64,7 +73,7 @@ class Login extends React.Component {
                                 href="https://play.google.com/store/apps/details?id=com.instagram.android&referrer=utm_source%3Dinstagramweb%26utm_campaign%3DloginPage%26ig_mid%3D252CCB11-1A79-40CC-AB09-E55D439CD8BB%26utm_content%3Dlo%26utm_medium%3Dbadge"><img
                                     alt="Google Play에서 이용 가능" className="googleplay_icon"
                                     src="https://www.instagram.com/static/images/appstore-install-badges/badge_android_korean-ko.png/f155b664a93b.png" /></a>
-                        </div>op-
+                        </div>
                     </div>
 
                 </main>
--- src/Pages/mihyeon/Main/Main.js
+++ src/Pages/mihyeon/Main/Main.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
+import MainFeed from "./Feed/MainFeed";
 import "./Main.scss";
 
 
 class Main extends React.Component {
-    render() {
-        return (
+    state = {
+        comment: '',
+        commentList: []
+    }
 
+    handleWriteComment = (e) => {
+        this.setState({
+            comment: e.target.value
+        })
+    }
+    handleUpdateComment = (e) => {
+        this.setState({
+            commentList: this.state.commentList.concat(this.state.comment)
+        })
+        console.log(this.state.commentList);
+    }
+
+
+    render() {
+        const { comment, commentList } = this.props;
+
+        return (
             <div className="westargram_whole_container">
                 <nav className="navigation_bar_container">
                     <div className="navigation_bar_column title_nav">
@@ -13,7 +33,7 @@ class Main extends React.Component {
                     </div>
 
                     <div className="navigation_bar_column input_search_container">
-                        <input type="text" className="input_search_bar" value="" placeholder="검색" />
+                        <input type="text" className="input_search_bar" defaultValue="" placeholder="검색" />
                         <i className="fas fa-search"></i>
                         <div className="searched_id_container">
                             <div className="searched_id_list">
@@ -26,82 +46,7 @@ class Main extends React.Component {
                         <img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/bearu/profile.png" alt="profile" />
                     </div>
                 </nav>
-                <main className="main_container">
-                    <div className="feed_container">
-                        <article className="feed_contents">
-                            <div className="feed_header">
-                                <img className="round_image"
-                                    src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/e35/s320x320/125347387_665294197692336_6026840706998274426_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=100&_nc_ohc=tTXjt-AKBPgAX_lD9lM&tp=1&oh=1400139d1b105c2d08f0a1bd8289d634&oe=602BF316"
-                                    alt="profile" />
-                                <div>
-                                    <p>Sia_fanpage</p>
-                                    <p>Sia, fanpage, Korea</p>
-                                </div>
-                                <i className="fas fa-ellipsis-h settings_icon"></i>
-                            </div>
-                            <div className="main_image_container">
-                                <img className="main-image"
-                                    src="https://scontent-gmp1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s640x640/59163523_155227472178890_919977553715347152_n.jpg?_nc_ht=scontent-gmp1-1.cdninstagram.com&_nc_cat=102&_nc_ohc=qQ3sW1xFc_kAX8Cyuyw&tp=1&oh=0f3eeb75a6b8f3eda3a9019bd5aa839e&oe=603189AF"
-                                    alt="sia" />
-                            </div>
-                            <div className="board_container">
-                                <div className="icons_contianer">
-                                    <i className="fas fa-heart"></i>
-                                    <i className="far fa-comment"></i>
-                                    <i className="fas fa-external-link-alt"></i>
-                                    <i className="far fa-bookmark"></i>
-                                </div>
-                                <div className="feed_icons_container">
-                                    <img className="round_image_small"
-                                        src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/e35/s320x320/125347387_665294197692336_6026840706998274426_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=100&_nc_ohc=tTXjt-AKBPgAX_lD9lM&tp=1&oh=1400139d1b105c2d08f0a1bd8289d634&oe=602BF316"
-                                        alt="profile" />
-                                    <span>Sia님 외 4명이 좋아합니다.</span>
-                                </div>
-                                <div className="feed_message_container">
-                                    <span className="feed_main_id">Sia_fanpage</span>
-                                    <span className="feed_message"> hoho!</span>
-                                </div>
-                                <div className="comment_conatainer">
-                                    <div className="comment_list">
-                                    </div>
-                                    <div className="comment_line"></div>
-                                    <form className="comment_write_form">
-                                        <textarea aria-label="댓글 달기 " className="comment_input" placeholder="댓글달기..."></textarea>
-                                        <button className="comment_submit_button" type="button">게시</button>
-                                    </form>
-                                </div>
-                            </div>
-                        </article>
-                    </div>
-                    <aside className="right_whole_container">
-                        <div className="main_right_container">
-                            <div className="recomandation_profile">
-                                <img className="round_image"
-                                    src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/e35/s320x320/125347387_665294197692336_6026840706998274426_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=100&_nc_ohc=tTXjt-AKBPgAX_lD9lM&tp=1&oh=1400139d1b105c2d08f0a1bd8289d634&oe=602BF316"
-                                    alt="profile" />
-                                <div className="own_profile_container">
-                                    <p>Sia_fanpage</p>
-                                    <p>Sia, fanpage, Korea</p>
-                                </div>
-                                <div>전환</div>
-                            </div>
-                            <div className="recomandation_list">
-                                <div>
-                                    <p>회원님을 위한 추천</p>
-                                    <p>모두 보기</p>
-                                </div>
-                                <div className="recomandation_individual">
-                                    <img class="round_image" src="https://scontent-gmp1-1.cdninstagram.com/v/t51.2885-15/e35/c157.0.405.405a/s320x320/137098773_710842363150812_7843511372978027429_n.jpg?_nc_ht=scontent-gmp1-1.cdninstagram.com&_nc_cat=100&_nc_ohc=DFlqa4zw7e4AX9wezXR&tp=1&oh=2bca11045c2e951d70138ca03d4ed95b&oe=6014AB36" alt="recomandation" />
-                                    <div>
-                                        <span className="user_id">hohohosia</span>
-                                        <span className="user_follow_status">hohohosia</span>
-                                    </div>
-                                    <div>팔로우</div>
-                                </div>
-                            </div>
-                        </div>
-                    </aside>
-                </main>
+                <MainFeed></MainFeed>
             </div>
         )
 
