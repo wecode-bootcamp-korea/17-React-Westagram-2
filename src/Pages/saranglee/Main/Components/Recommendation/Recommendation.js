83e0fc67261e7f059e0514caa984466111da1364
rosarang <72490935+rosarang@users.noreply.github.com>
2021-02-04 12:15:09 +0900

    Add: Story, Recommendation 컴포넌트 mock data 추가

--- src/Pages/saranglee/Main/Components/Recommendation/Recommendation.js
+++ src/Pages/saranglee/Main/Components/Recommendation/Recommendation.js
@@ -4,15 +4,16 @@ import './Recommendation.scss';
 
 class Recommendation extends Component {
   render() {
+    const { data } = this.props;
     return (
       <div className="recommended_account">
         <div className="img_user_info">
-          <a className="aside_img" href="https://www.instagram.com/onearthmagazine/">
-            <img alt="Profile" className="user_profile_img img_medium" src="https://scontent-gmp1-1.cdninstagram.com/v/t51.2885-19/s150x150/45800260_2095307027159510_8434008982594519040_n.jpg?_nc_ht=scontent-gmp1-1.cdninstagram.com&_nc_ohc=MKsMjBc7NOcAX-e6MmW&tp=1&oh=385bb59b283c83b21fa182c7c77f7626&oe=6036B7D4" />
+          <a className="aside_img" href={data.userLink}>
+            <img alt="Profile" className="user_profile_img img_medium" src={data.userImage} />
           </a>
           <div className="user_id_info">
-            <span className="user_id">onearthmagazine</span>
-            <span className="aside_user_info">dribble님이 팔로우합니다</span>
+            <span className="user_id">{data.userId}</span>
+            <span className="aside_user_info">{data.follower}</span>
           </div>
         </div>
         <button className="follow btn">팔로우</button>
